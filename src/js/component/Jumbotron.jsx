a4385180eefca1dfaf0a2d6a1397136bd996f4dc
Robert <155775143+Roberttovarv@users.noreply.github.com>
2024-04-15 18:42:30 +0000

    First

---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Jumbotron = () => {
+    return <div className="jumbotron bg-secondary bg-opacity-25 py-3">
+        <h1 className="display-4"><b>Madrid's best burgers!</b></h1>
+        <p className="lead">Con un sabor inigualable y una textura nunca antes vista.</p>
+        <p>Traemos un concepto revolucionario para brindar al usuario una experiencia inolvidable</p>
+        
+        <a className="btn btn-warning btn-lg" href="#" role="button">Nuestra carta</a>
+    </div>
+  };
